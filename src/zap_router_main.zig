9540df063e4518a7c3a43ded0c129d3cb3047da5
Walt Chen <godsarmycy@gmail.com>
2025-04-24 23:02:01 -0400

    zap route example fix

--- src/zap_router_main.zig
+++ src/zap_router_main.zig
@@ -3,30 +3,46 @@ const std = @import("std");
 const m = @import("metrics");
 const zap = @import("zap");
 
-const Allocator = std.mem.Allocator;
-
+const HitLabel = struct { client: []const u8 };
 const Metrics = struct {
+    const Hits = m.CounterVec(u64, HitLabel);
+    const Accessed = m.Gauge(u64);
+
     // counter to calculate how many hit to URL
-    hits: m.Counter(u32),
+    hits: Hits,
+    // gauge for latest accessed timestamp in epoch
+    accessed: Accessed,
 };
 
+pub fn info2addr(info_s: zap.fio.fio_str_info_s) []const u8 {
+    if (info_s.data == 0)
+        return "";
+    return info_s.data[0..info_s.len];
+}
+
 pub const WebPackage = struct {
-    allocator: Allocator,
+    allocator: std.mem.Allocator,
     metrics: Metrics,
 
-    pub fn init(allocator: Allocator) WebPackage {
+    pub fn init(allocator: std.mem.Allocator) WebPackage {
         // initialize Metrics object as self.metrics
         return .{
             .allocator = allocator,
             .metrics = .{
-                .hits = m.Counter(u32).init("hits", .{}, .{}),
+                .hits = try Metrics.Hits.init(allocator, "hits", .{}, .{}),
+                .accessed = Metrics.Accessed.init("accessed", .{}, .{}),
             },
         };
     }
 
     pub fn getMetrics(self: *WebPackage, req: zap.Request) !void {
         // increase counter first when URL is hit
-        self.metrics.hits.incr();
+        const addr_info_s = zap.fio.http_peer_addr(req.h);
+        const client = info2addr(addr_info_s);
+
+        const labels: HitLabel = .{ .client = client };
+        try self.metrics.hits.incr(labels);
+        self.metrics.accessed.set(@intCast(std.time.milliTimestamp()));
 
         // buffer for output
         var arr = std.ArrayList(u8).init(self.allocator);
@@ -55,6 +71,8 @@ pub fn main() !void {
     defer simpleRouter.deinit();
 
     var webPackage = WebPackage.init(allocator);
+    defer webPackage.metrics.hits.deinit();
+
     try simpleRouter.handle_func("/metrics", &webPackage, &WebPackage.getMetrics);
 
     var listener = zap.HttpListener.init(.{
@@ -74,7 +92,14 @@ pub fn main() !void {
 
 test "simple test" {
     var webPackage = WebPackage.init(std.testing.allocator);
-    try std.testing.expectEqual(webPackage.metrics.hits.impl.count, 0);
-    webPackage.metrics.hits.incr();
-    try std.testing.expectEqual(webPackage.metrics.hits.impl.count, 1);
+    defer webPackage.metrics.hits.deinit();
+
+    try std.testing.expectEqual(webPackage.metrics.accessed.impl.value, 0);
+
+    const labels: HitLabel = .{ .client = "foo" };
+    try webPackage.metrics.hits.incr(labels);
+    webPackage.metrics.accessed.set(100);
+
+    try std.testing.expectEqual(webPackage.metrics.hits.impl.values.get(labels).?.count, 1);
+    try std.testing.expectEqual(webPackage.metrics.accessed.impl.value, 100);
 }
